fe230d8d8c67f5b05c54e7cc391f8e68b674097a
SonuBardai <sonubardai9@gmail.com>
2023-06-22 21:40:27 +0530

    Create a websockets server in actix-web

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,54 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix::{Actor, StreamHandler};
+use actix_web::{
+    get, middleware::Logger, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_web_actors::ws;
+use env_logger::Env;
+use log::info;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     "Hello world".to_owned()
 }
 
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, item: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        if let Ok(ws::Message::Text(text)) = item {
+            info!("Text received from websockets connection: {text}");
+            ctx.text(text)
+        }
+    }
+}
+
+#[get("/ws")]
+async fn web_socket(
+    request: HttpRequest,
+    stream: web::Payload,
+) -> Result<HttpResponse, actix_web::Error> {
+    if let Some(host) = request.headers().get("host") {
+        info!("Websockets connection established with host: {:?}", host);
+    } else {
+        info!("Websockets connection established");
+    }
+    ws::start(MyWs, &request, stream)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(hello)
+            .service(web_socket)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
