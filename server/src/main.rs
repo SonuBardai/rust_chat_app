d24378d656b56928e62179c1faaf4dc13224c9b1
SonuBardai <sonubardai9@gmail.com>
2023-06-23 11:17:12 +0530

    Fix shared sonubardai vec by using app_data

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use actix::{Actor, StreamHandler};
 use actix_web::{get, middleware::Logger, web, App, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
@@ -5,7 +7,7 @@ use env_logger::Env;
 use log::info;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 enum MessageType {
     UserConnect,
     UserDisconnect,
@@ -13,7 +15,7 @@ enum MessageType {
     Error,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Message {
     message_type: MessageType,
     payload: String,
@@ -47,12 +49,12 @@ impl User {
 }
 
 struct MyWs {
-    users: Vec<User>,
+    users: Arc<Mutex<Vec<User>>>,
 }
 
 impl MyWs {
-    fn new() -> Self {
-        Self { users: Vec::new() }
+    fn new(users: Arc<Mutex<Vec<User>>>) -> Self {
+        Self { users }
     }
 }
 
@@ -64,17 +66,18 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
     fn handle(&mut self, item: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         if let Ok(ws::Message::Text(text)) = item {
             if let Ok(message) = serde_json::from_str::<Message>(&text) {
+                info!("Message: {message:?}");
                 match message.message_type {
                     MessageType::UserConnect => {
-                        let user = User::new(self.users.len() as u32, message.payload);
-                        self.users.push(user);
+                        let user =
+                            User::new(self.users.lock().unwrap().len() as u32, message.payload);
+                        self.users.lock().unwrap().push(user);
                     }
                     MessageType::Text => {}
                     _ => {
                         return ctx.text(Message::serde_error("Unknown message received"));
                     }
                 }
-                info!("USERS: {:?}", self.users);
             } else {
                 return ctx.text(Message::serde_error("Failed to extract text"));
             }
@@ -86,6 +89,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
 async fn web_socket(
     request: HttpRequest,
     stream: web::Payload,
+    data: web::Data<AppState>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let host = request
         .headers()
@@ -95,14 +99,32 @@ async fn web_socket(
     let ip = request.connection_info().peer_addr().unwrap().to_owned();
     let time = chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
     info!("Websockets connection established with host ({host:?}) and ip ({ip}) - {time}");
-    ws::start(MyWs::new(), &request, stream)
+    ws::start(MyWs::new(data.users.clone()), &request, stream)
+}
+
+struct AppState {
+    users: Arc<Mutex<Vec<User>>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            users: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-    HttpServer::new(move || App::new().wrap(Logger::default()).service(web_socket))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
+    let app_state = web::Data::new(AppState::new());
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(app_state.clone())
+            .service(web_socket)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
